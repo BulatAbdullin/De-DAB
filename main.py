0375e634472ff1bac4e2ff31c5b3c05bb740ba5c
Bulat Abdullin <bulat.abdullin@stud.cs.msu.ru>
2021-04-27 21:42:15 +0300

    Differentiate Hematoxylin-stained tissues
    + Convert De-DAB-image to Lab space
    
    + Uneven quantization of the b component
    
    + Blue filter, Hematoxylin-only image (use value before the peak)
    
    Fixed:
    
    revised_hist_centers in
    get_n_classes_image_histogram(bn, num_intervals=10)
    now contains original hisogram centers not the indices

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 import sys
 import numpy as np
 import skimage.io as skio
+import skimage.color as skcolor
 import skimage.util as skutil
 import skimage.exposure as skexposure
+
 
 def rgb2bn(img, alpha=0.5, beta=0.25):
     # BN = Blue - alpha*Red - beta*Green
@@ -16,14 +18,13 @@ def rgb2bn(img, alpha=0.5, beta=0.25):
 def get_n_classes_image_histogram(bn, num_intervals=10):
     hist, hist_centers = skexposure.histogram(bn)
     # Calculate revised histogram
-    num_intervals = 10
     interval_len = len(hist) // num_intervals
     revised_hist = np.empty(num_intervals)
     revised_hist_centers = np.empty(num_intervals)
     for i in range(num_intervals):
         j = i * interval_len
         revised_hist[i] = np.max(hist[j: j+interval_len])
-        revised_hist_centers[i] = j + np.argmax(hist[j: j+interval_len])
+        revised_hist_centers[i] = hist_centers[j + np.argmax(hist[j: j+interval_len])]
     return revised_hist, revised_hist_centers
 
 
@@ -45,6 +46,16 @@ def get_brown_filter(img):
     return (bn < threshold)[..., np.newaxis]
 
 
+def get_blue_filter(de_dab):
+    lab = skcolor.rgb2lab(de_dab)
+    b = lab[..., 2] - np.min(lab[..., 2])
+
+    revised_hist, revised_hist_centers = get_n_classes_image_histogram(b)
+    # set the cut-off point before the peak
+    threshold = revised_hist_centers[np.argmax(revised_hist) - 1]
+    return (b < threshold)[..., np.newaxis]
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: " + sys.argv[0] + " input_image output_dab output_hematoxylin")
@@ -53,5 +64,9 @@ if __name__ == "__main__":
     tissue = skutil.img_as_float(skio.imread(sys.argv[1]))
     brown_filter = get_brown_filter(tissue)
     dab = tissue * brown_filter
+    de_dab = tissue * (brown_filter == False)
 
-    skio.imsave(sys.argv[2], skutil.img_as_ubyte(dab))
+    blue_filter = get_blue_filter(de_dab)
+    hematoxylin = de_dab * blue_filter
+
+    skio.imsave(sys.argv[2], skutil.img_as_ubyte(hematoxylin))
